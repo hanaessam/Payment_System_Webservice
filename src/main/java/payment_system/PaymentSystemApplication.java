31471fff6cd1631f1b972e3f4162d15e750f1997
hanaessam <hanaessamsayed@gmail.com>
2022-12-27 03:04:58 +0200

    payment webservice

--- src/main/java/payment_system/PaymentSystemApplication.java
+++ src/main/java/payment_system/PaymentSystemApplication.java
@@ -5,7 +5,7 @@ package payment_system;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-@SpringBootApplication(scanBasePackages = { "refunds", "users", "security"})
+@SpringBootApplication(scanBasePackages = { "refunds", "users", "security", "payment"})
 public class PaymentSystemApplication {
 
 	public static void main(String[] args) {
